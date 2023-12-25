c334200d7f342a0b296d9d91fdd5e6f58bd14533
nine96as <ashrafsaid79@gmail.com>
2023-12-25 23:03:16 +0000

    Nest page content into main elem

--- client/src/layout/NavBar/index.jsx
+++ client/src/layout/NavBar/index.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import './style.css';
 
 const NavBar = () => {
   return (
-    <header>
-      <a href='' className='logo'>
-        MyBlog
-      </a>
-      <nav>
-        <Link to='/login'>Login</Link>
-        <Link to=''>Register</Link>
-      </nav>
-    </header>
+    <main>
+      <header>
+        <a href='/' className='logo'>
+          MyBlog
+        </a>
+        <nav>
+          <Link to='/login'>Login</Link>
+          <Link to=''>Register</Link>
+        </nav>
+      </header>
+      <Outlet />
+    </main>
   );
 };
 
