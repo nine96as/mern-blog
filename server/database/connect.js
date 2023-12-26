50b6f5602afcca89b24e287c09820228069b7480
nine96as <ashrafsaid79@gmail.com>
2023-12-26 21:44:53 +0000

    Revamp db connection logic

---
+++ server/database/connect.js
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import mongoose from 'mongoose';
+
+export const connectDB = () => {
+  const url = process.env.DB_URL;
+
+  try {
+    mongoose.connect(url);
+  } catch (e) {
+    console.log(e.message);
+    process.exit(1);
+  }
+
+  const dbConnection = mongoose.connection;
+  dbConnection.once('open', (_) => console.log(`DB connected successfully`));
+  dbConnection.on('error', (e) => console.log(`DB connection error: ${e}`));
+
+  return;
+};
