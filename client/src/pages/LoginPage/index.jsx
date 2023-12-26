6a25e87e73f09cbc7749d0898836a75ba76abd75
nine96as <ashrafsaid79@gmail.com>
2023-12-26 15:28:29 +0000

    Add h1s to Login, RegisterPage cmpts

--- client/src/pages/LoginPage/index.jsx
+++ client/src/pages/LoginPage/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const LoginPage = () => {
   return (
-    <form action=''>
+    <form className='login'>
+      <h1>Login</h1>
       <input type='text' name='' placeholder='username' />
       <input type='password' name='' placeholder='password' />
       <button>Login</button>
--- client/src/pages/RegisterPage/index.jsx
+++ client/src/pages/RegisterPage/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const RegisterPage = () => {
   return (
-    <form action=''>
+    <form className='register'>
+      <h1>Register</h1>
       <input type='text' name='' placeholder='username' />
       <input type='password' name='' placeholder='password' />
       <button>Register</button>
