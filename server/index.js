6a69d5f81bc43a17288b5d1c74a5da6b53930731
nine96as <ashrafsaid79@gmail.com>
2023-12-26 21:44:40 +0000

    Rm db connection logic

--- server/index.js
+++ server/index.js
@@ -1,12 +1,6 @@
 import 'dotenv/config';
-import mongoose from 'mongoose';
 import { app } from './app.js';
 
-mongoose
-  .connect(process.env.DB_URL)
-  .then(() => {
-    app.listen(process.env.PORT, () =>
-      console.log(`Server ready on: http://localhost:${process.env.PORT}`)
-    );
-  })
-  .catch((e) => console.log(e));
+app.listen(process.env.PORT, () => {
+  console.log(`Server ready on: http://localhost:${process.env.PORT}`);
+});
