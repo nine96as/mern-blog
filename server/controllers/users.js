9f220335d09569b323641567d4215066930e2b73
nine96as <ashrafsaid79@gmail.com>
2023-12-26 21:13:56 +0000

    Revamp register() logic, implement login() logic

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,37 +1,56 @@
 import bcrypt from 'bcrypt';
+import 'dotenv/config';
+import jwt from 'jsonwebtoken';
 import { User } from '../models/User.js';
 
 export const register = async (req, res) => {
   try {
-    const data = req.body;
+    const { username, password } = req.body;
 
     const salt = await bcrypt.genSalt(parseInt(process.env.BCRYPT_SALT_ROUNDS));
-    data.password = await bcrypt.hash(data.password, salt);
 
-    const result = await User.create(data);
+    const result = await User.create({
+      username: username,
+      password: await bcrypt.hash(password, salt)
+    });
     res.status(201).json(result);
   } catch (e) {
-    res.status(400).json({ error: e.message });
+    res.status(500).json({ error: e.message });
   }
 };
 
 export const login = async (req, res) => {
-  const data = req.body;
   try {
-    const user = await User.findOne({ username: data.username });
+    const { username, password } = req.body;
+    const user = await User.findOne({ username: username });
 
-    if (!user) throw new Error('User not found');
+    if (!user) return res.status(401).json({ error: 'User not found' });
 
-    const authenticated = await bcrypt.compare(data.password, user.password);
+    const authenticated = await bcrypt.compare(password, user.password);
 
     if (!authenticated) {
-      throw new Error('Incorrect credentials.');
+      return res.status(401).json({ error: 'Incorrect credentials' });
     } else {
-      const token = await Token.create({ token: uuidv4(), userId: user._id });
-      res.status(200).json({ authenticated: true, token: token.token });
+      const token = jwt.sign(
+        { userId: user._id },
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' },
+        (e, token) => {
+          if (e) return res.status(500).json({ error: e.message });
+
+          res
+            .cookie('token', token, {
+              maxAge: 60 * 60 * 1000, // 1 hour
+              httpOnly: true,
+              secure: true,
+              sameSite: true
+            })
+            .json('ok');
+        }
+      );
     }
   } catch (e) {
-    res.status(403).json({ error: e.message });
+    res.status(500).json({ error: e.message });
   }
 };
 
