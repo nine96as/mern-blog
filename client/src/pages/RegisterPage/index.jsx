af09c09f54aa17c60a783fa60ece921f92fa4401
nine96as <ashrafsaid79@gmail.com>
2023-12-26 13:52:18 +0000

    Create RegisterPage markup skeleton

---
+++ client/src/pages/RegisterPage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RegisterPage = () => {
+  return (
+    <form action=''>
+      <input type='text' name='' id='' />
+      <input type='password' name='' id='' />
+      <button>Register</button>
+    </form>
+  );
+};
+
+export default RegisterPage;
