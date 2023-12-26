110053f1110bebcafd49316453a9acab9ccd9e6e
nine96as <ashrafsaid79@gmail.com>
2023-12-26 13:54:22 +0000

    Add /register route, link RegisterPage cmpt

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
       <Route path='/' element={<NavBar />}>
         <Route index element={<Pages.HomePage />} />
         <Route path='/login' element={<Pages.LoginPage />} />
+        <Route path='/register' element={<Pages.RegisterPage />} />
       </Route>
     </Routes>
   );
