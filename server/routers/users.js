d66d738ad892bf9d949890f3a0adcfd97cf5c250
nine96as <ashrafsaid79@gmail.com>
2023-12-27 18:46:39 +0000

    Add refs to auth middleware in /profile

--- server/routers/users.js
+++ server/routers/users.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import * as userController from '../controllers/users.js';
+import { authenticateToken } from '../middleware/auth.js';
 
 const userRouter = Router();
 
 userRouter.post('/register', userController.register);
 userRouter.post('/login', userController.login);
+userRouter.get('/profile', authenticateToken, userController.profile);
 userRouter.get('/logout', userController.logout);
 
 export default userRouter;
