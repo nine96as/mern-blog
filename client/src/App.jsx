fbd60c97af71096406210dd288a57eb37c5ec3a1
nine96as <nine96@skiff.com>
2024-01-24 14:16:16 +0000

    Add routing for index and /login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Post } from './components';
 import { NavBar } from './layout';
 
 const App = () => {
   return (
-    <main>
-      <NavBar />
-      <Post />
-      <Post />
-      <Post />
-    </main>
+    <Routes>
+      <Route
+        index
+        element={
+          <main>
+            <NavBar />
+            <Post />
+            <Post />
+            <Post />
+          </main>
+        }
+      />
+
+      <Route path='/login' element={<div>login</div>} />
+    </Routes>
   );
 };
 
