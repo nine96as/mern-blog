a5bd13127a0edc1601559e1f69d81d9e01aa7cb5
nine96as <nine96@skiff.com>
2024-01-24 14:16:15 +0000

    Move related markup to Post cmpt

---
+++ client/src/components/Post/index.jsx
@@ -0,0 +1,24 @@
+const Post = () => {
+  return (
+    <div className='post'>
+      <div className='image'>
+        <img
+          src='https://techcrunch.com/wp-content/uploads/2023/12/CMC_7587.jpg?w=1390&crop=1'
+          alt=''
+        />
+      </div>
+      <div className='content'>
+        <h2>Amazon’s new Echo Frames can’t touch the Ray-Ban Meta</h2>
+        <p className='info'>
+          <a className='author'>nine96as</a>
+          <time>25-12-2023 16:45</time>
+        </p>
+        <p className='summary'>
+          The new glasses feature improved sound, but still mostly fall short
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
