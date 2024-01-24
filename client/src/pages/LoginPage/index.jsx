90c79dfc05eafafc9df978d07ed08b383e3e05b9
nine96as <nine96@skiff.com>
2024-01-24 14:16:19 +0000

    Implement handleSubmit() logic

--- client/src/pages/LoginPage/index.jsx
+++ client/src/pages/LoginPage/index.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch('http://localhost:3000/users/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      }),
+      credentials: 'include'
+    });
+
+    if (!response.ok) {
+      alert(`Login failed`);
+    } else {
+      alert(`Login successful`);
+      navigate('/');
+    }
+  };
+
   return (
-    <form className='login'>
+    <form className='login' onSubmit={handleSubmit}>
       <h1>Login</h1>
-      <input type='text' name='' placeholder='username' />
-      <input type='password' name='' placeholder='password' />
+      <input
+        type='text'
+        required
+        placeholder='username'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type='password'
+        required
+        placeholder='password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button>Login</button>
     </form>
   );
