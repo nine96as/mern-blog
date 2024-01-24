c71338b605bdb06b0e1e23fe365aae6f9f112b78
nine96as <nine96@skiff.com>
2024-01-24 14:16:18 +0000

    Add server skeleton code

---
+++ server/app.js
@@ -0,0 +1,17 @@
+import cors from 'cors';
+import express from 'express';
+import logger from 'morgan';
+
+export const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(logger('dev'));
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Blog API',
+    description: 'TBC',
+    endpoints: []
+  });
+});
