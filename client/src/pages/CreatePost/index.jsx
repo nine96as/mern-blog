7bd2a55ed0cd110d3a83e981d12984c4520732c5
nine96as <nine96@skiff.com>
2024-02-07 20:53:06 +0000

    Add Editor into CreatePost page

--- client/src/pages/CreatePost/index.jsx
+++ client/src/pages/CreatePost/index.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Editor } from '../../components'
+import './style.css'
 
 const CreatePost = () => {
-  return <div>CreatePost</div>
+  return (
+    <form className='create'>
+      <div className='create-metadata'>
+        <input className='create' type='title' placeholder='title' />
+        <input className='create' type='summary' placeholder='summary' />
+        <input className='create' type='file' name='' id='' />
+      </div>
+      <div className='create-body'>
+        <Editor />
+      </div>
+
+      <button className='create'>create</button>
+    </form>
+  )
 }
 
 export default CreatePost
