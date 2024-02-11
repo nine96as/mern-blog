5e9314607ef918ed9b000088d9aa67ca544d2f94
nine96as <nine96@skiff.com>
2024-02-11 14:19:24 +0000

    Revamp importing of userRouter funcs

--- server/routers/users.js
+++ server/routers/users.js
@@ -1,12 +1,12 @@
 import { Router } from 'express'
-import * as userController from '../controllers/users.js'
+import { login, logout, profile, register } from '../controllers/users.js'
 import { authenticateToken } from '../middleware/auth.js'
 
 const userRouter = Router()
 
-userRouter.post('/register', userController.register)
-userRouter.post('/login', userController.login)
-userRouter.post('/logout', userController.logout)
-userRouter.get('/profile', authenticateToken, userController.profile)
+userRouter.post('/register', register)
+userRouter.post('/login', login)
+userRouter.post('/logout', logout)
+userRouter.get('/profile', authenticateToken, profile)
 
 export default userRouter
