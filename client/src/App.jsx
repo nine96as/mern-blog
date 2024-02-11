dadf94d327f0a3c7b053c476def9b8c0e2ecdc8c
nine96as <nine96@skiff.com>
2024-02-11 14:25:23 +0000

    Add Toaster to App cmpt

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
+import { Toaster } from 'sonner'
 import './App.css'
 import { UserContextProvider } from './contexts/UserContext'
 import { NavBar } from './layout'
@@ -7,12 +8,13 @@ import * as Pages from './pages'
 const App = () => {
   return (
     <UserContextProvider>
+      <Toaster richColors closeButton />
       <Routes>
         <Route path='/' element={<NavBar />}>
           <Route index element={<Pages.HomePage />} />
           <Route path='/login' element={<Pages.LoginPage />} />
           <Route path='/register' element={<Pages.RegisterPage />} />
-          <Route path='/create' element={<Pages.CreatePost />} />
+          <Route path='/create' element={<Pages.CreatePostPage />} />
         </Route>
       </Routes>
     </UserContextProvider>
