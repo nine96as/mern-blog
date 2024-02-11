de09fa861a0f108959bb5d53def8d515a2541793
nine96as <nine96@skiff.com>
2024-02-11 14:19:53 +0000

    Add Post model logic

---
+++ server/models/Post.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+const postSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    summary: {
+      type: String,
+      required: true,
+    },
+    cover: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+export const Post = mongoose.model('Post', postSchema)
