ffde51c84913cdc81ca68385d6ddbde3a140a6cd
nine96as <nine96@skiff.com>
2024-02-12 12:17:07 +0000

    Complete Post cmpt logic with props

--- client/src/components/Post/index.jsx
+++ client/src/components/Post/index.jsx
@@ -1,26 +1,30 @@
-import './style.css';
+import { formatISO9075 } from 'date-fns'
+import { Link } from 'react-router-dom'
+import './style.css'
 
-const Post = () => {
+const Post = ({ _id, title, summary, cover, createdAt, author }) => {
   return (
     <div className='post'>
       <div className='image'>
-        <img
-          src='https://techcrunch.com/wp-content/uploads/2023/12/CMC_7587.jpg?w=1390&crop=1'
-          alt=''
-        />
+        <Link to={`/posts/${_id}`}>
+          <img
+            src={`http://localhost:3000/${cover}`}
+            alt={`${title} cover image`}
+          />
+        </Link>
       </div>
       <div className='content'>
-        <h2>Amazon’s new Echo Frames can’t touch the Ray-Ban Meta</h2>
+        <Link to={`/posts/${_id}`}>
+          <h2>{title}</h2>
+        </Link>
         <p className='info'>
-          <a className='author'>nine96as</a>
-          <time>25-12-2023 16:45</time>
+          <a className='author'>{author.username}</a>
+          <time>{formatISO9075(new Date(createdAt))}</time>
         </p>
-        <p className='summary'>
-          The new glasses feature improved sound, but still mostly fall short
-        </p>
+        <p className='summary'>{summary}</p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Post;
+export default Post
