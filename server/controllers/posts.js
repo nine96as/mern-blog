f8565ea065364d721b4b74375ae6a59c497b7196
nine96as <nine96@skiff.com>
2024-02-12 12:08:33 +0000

    Add completed index and show logic

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,6 +1,28 @@
 import fs from 'fs'
 import { Post } from '../models/Post.js'
 
+export const index = async (req, res) => {
+  try {
+    const posts = await Post.find()
+      .populate('author', ['username'])
+      .sort({ createdAt: -1 })
+      .limit(10)
+    res.status(200).json(posts)
+  } catch (e) {
+    res.status(500).json({ error: e.message })
+  }
+}
+
+export const show = async (req, res) => {
+  try {
+    const { id } = req.params
+    const post = await Post.findById(id).populate('author', ['username'])
+    res.status(200).json(post)
+  } catch (e) {
+    res.status(500).json({ error: e.message })
+  }
+}
+
 export const create = async (req, res) => {
   try {
     const { originalname, path } = req.file
@@ -18,7 +40,7 @@ export const create = async (req, res) => {
       summary,
       content,
       cover: newPath,
-      userId,
+      author: userId,
     })
 
     res.status(201).json(result)
