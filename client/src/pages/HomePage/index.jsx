baef443881ee2fe1d51ddd06177ca64dcd50cbd6
nine96as <nine96@skiff.com>
2024-02-12 12:18:10 +0000

    Complete HomePage logic

--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from 'react'
+import { toast } from 'sonner'
 import { Post } from '../../components'
 
 const HomePage = () => {
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:3000/posts').then((res) => {
+      if (res.ok) {
+        res.json().then((data) => setPosts(data))
+      } else {
+        toast.error(`failed to fetch posts`)
+      }
+    })
+  }, [])
+
   return (
     <>
-      <Post />
-      <Post />
-      <Post />
+      {posts.length > 0 ? (
+        posts.map((post) => <Post key={post._id} {...post} />)
+      ) : (
+        <p>no posts yet...</p>
+      )}
     </>
   )
 }
