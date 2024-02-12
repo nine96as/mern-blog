96638aa3ba46b968a901800a0dd81ba22be28648
nine96as <nine96@skiff.com>
2024-02-12 12:09:02 +0000

    Add index and show refs

--- server/routers/posts.js
+++ server/routers/posts.js
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import multer from 'multer'
-import { create } from '../controllers/posts.js'
+import { create, index, show } from '../controllers/posts.js'
 import { authenticateToken } from '../middleware/auth.js'
 
 const postRouter = Router()
 
 const uploadMiddleware = multer({ dest: 'uploads/' })
 
+postRouter.get('/', index)
+postRouter.get('/:id', show)
 postRouter.post('/', authenticateToken, uploadMiddleware.single('file'), create)
 
 export default postRouter
